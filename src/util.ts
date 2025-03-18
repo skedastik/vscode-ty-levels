24d069a0024cc40e140cbb15222a2bed89765334
Alaric Holloway <alaric.holloway@gmail.com>
2025-03-18 17:19:27 -0400

    Use more robust logic to detect numeric strings

--- src/util.ts
+++ src/util.ts
@@ -5,3 +5,5 @@ export const debounce = (fn: Function, delay: number) => {
         timer = setTimeout(() => fn(...args), delay);
     };
 };
+
+export const isNumericString = (s: string) => /^\s*-?\s*[0-9]+(\.[0-9]+)?(e-?[0-9]+)?\s*$/.test(s);
